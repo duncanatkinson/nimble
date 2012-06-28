3f115a2ecdc0130f528c7623f883de4bc16c19eb
Duncan Atkinson <duncan.atkinson@gmail.com>
2012-06-29 00:00:49 +0100

    switched to new selenium web driver and now only testing with chrome

--- test-nimble/src/test/java/com/felthat/testnimble/InputFieldIT.java
+++ test-nimble/src/test/java/com/felthat/testnimble/InputFieldIT.java
@@ -1,81 +1,92 @@
 package com.felthat.testnimble;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class InputFieldIT {
 
-	private List<WebDriver> webDrivers;
-	
-	@Before
-	public void setup(){
-		webDrivers = new ArrayList<WebDriver>();
-		FirefoxProfile profile = new FirefoxProfile();
-		webDrivers.add(new FirefoxDriver(profile));
-		webDrivers.add(new InternetExplorerDriver());
-	}
-	
-	@After
-	public void cleanUp(){
-		for(WebDriver d:webDrivers){
-			try{
-			d.quit();
-			}catch (Exception e) {
-			}
-		}
-	}
+	  private static ChromeDriverService service;
+	  private WebDriver driver;
+
+	  @BeforeClass
+	  public static void createAndStartService() throws IOException {
+	    service = ChromeDriverService.createDefaultService();
+	    service.start();
+	  }
+
+	  @AfterClass
+	  public static void createAndStopService() {
+	    service.stop();
+	  }
+
+	  @Before
+	  public void createDriver() {
+	    driver = new RemoteWebDriver(service.getUrl(),
+	        DesiredCapabilities.chrome());
+	  }
+
+	  @After
+	  public void quitDriver() {
+	    driver.close();
+	  }
 	
 	
 	@Test
-	public void checkRenderingAndStore(){
-		for(WebDriver driver : webDrivers){
-			System.out.println(driver.getTitle());
-			driver.get("http://localhost:9090/test-nimble/index.jsp");
-			WebElement field = driver.findElement(By.id("name"));
-			field.sendKeys("Mr Cheese");
-			field.sendKeys(Keys.TAB);
-			field = driver.switchTo().activeElement();
-			field.sendKeys("21");
-			field.sendKeys(Keys.TAB);
-			field = driver.switchTo().activeElement();
-			field.sendKeys("Male");
-			field.sendKeys(Keys.TAB);
-			field = driver.switchTo().activeElement();
-			field.sendKeys(Keys.ARROW_DOWN);
-			field.sendKeys(Keys.ARROW_DOWN);
-			field.sendKeys(Keys.TAB);
-			
-			waitForAjaxFinished(driver);
-			
-			driver.navigate().refresh();
-			
-			field = driver.findElement(By.id("name"));
-			assertEquals("Mr Cheese",field.getAttribute("value"));
-			field = driver.findElement(By.id("age"));
-			assertEquals("21",field.getAttribute("value"));
-			field = driver.findElement(By.id("sex"));
-			assertEquals("Male",field.getAttribute("value"));
-			field = driver.findElement(By.id("make"));
-			assertEquals("Porsche",field.getAttribute("value"));
-			driver.quit();
-		}
+	public void checkRendering(){
+		driver.get("http://localhost:9090/test-nimble/index.jsp");
+		WebElement field = driver.findElement(By.id("name"));
+		field.sendKeys("TESTIING");
+		assertTrue(driver.getPageSource().contains("<h1>Text Field Tests</h1>"));
 	}
 
+	@Test
+	public void checkRenderingAndStore(){
+		System.out.println(driver.getTitle());
+		driver.get("http://localhost:9090/test-nimble/index.jsp");
+		WebElement field = driver.findElement(By.id("name"));
+		field.sendKeys("Mr Cheese");
+		field.sendKeys(Keys.TAB);
+		field = driver.switchTo().activeElement();
+		field.sendKeys("21");
+		field.sendKeys(Keys.TAB);
+		field = driver.switchTo().activeElement();
+		field.sendKeys("Male");
+		field.sendKeys(Keys.TAB);
+		field = driver.switchTo().activeElement();
+		field.sendKeys(Keys.ARROW_DOWN);
+		field.sendKeys(Keys.ARROW_DOWN);
+		field.sendKeys(Keys.TAB);
+		
+		waitForAjaxFinished(driver);
+		
+		driver.navigate().refresh();
+		
+		field = driver.findElement(By.id("name"));
+		assertEquals("Mr Cheese",field.getAttribute("value"));
+		field = driver.findElement(By.id("age"));
+		assertEquals("21",field.getAttribute("value"));
+		field = driver.findElement(By.id("sex"));
+		assertEquals("Male",field.getAttribute("value"));
+		field = driver.findElement(By.id("make"));
+		assertEquals("Porsche",field.getAttribute("value"));
+	}
 	private void waitForAjaxFinished(WebDriver driver) {
 		new WebDriverWait(driver, 5).until(new ExpectedCondition<WebElement>(){
 					@Override
