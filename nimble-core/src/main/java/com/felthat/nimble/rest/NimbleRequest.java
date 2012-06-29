8217cdd107518f84a070718796d8eaf9a2ba3db9
Duncan Atkinson <duncan.atkinson@gmail.com>
2012-06-29 19:25:38 +0100

    separate data mapping for field updates than whole graphs

--- nimble-core/src/main/java/com/felthat/nimble/rest/NimbleRequest.java
+++ nimble-core/src/main/java/com/felthat/nimble/rest/NimbleRequest.java
@@ -1,6 +1,7 @@
 package com.felthat.nimble.rest;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
 
 public class NimbleRequest implements Serializable {
@@ -8,6 +9,15 @@ public class NimbleRequest implements Serializable {
 	private static final long serialVersionUID = 6320709312448457036L;
 	
 	private Map<String,Object> data;
+	private List<String> field;
+
+	public List<String> getField() {
+		return field;
+	}
+
+	public void setField(List<String> field) {
+		this.field = field;
+	}
 
 	public Map<String, Object> getData() {
 		return data;
